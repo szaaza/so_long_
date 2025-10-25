e8eee5e443955119a7fa4eec54c512f396df5fce
Sasan Zanjani <sazanjan@2-H-1.42heilbronn.de>
2025-10-25 16:20:01 +0200

    add so_long.h file

---
+++ so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sazanjan <<sazanjan@student.42heilbronn    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 16:17:03 by sazanjan          #+#    #+#             */
+/*   Updated: 2025/10/25 16:17:52 by sazanjan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//make .h file for so_long project
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "mlx.h"
+# include "libft.h"
+# include "get_next_line.h"
+# define TILE_SIZE 32
