8c635a8fa3b514ad7a6e7780a88acea89ba0d2ef
Sasan Zanjani <sazanjan@1-A-32.42heilbronn.de>
2025-10-26 14:21:12 +0100

    add the checkfile funciton in main with header, utils

--- so_long.h
+++ so_long.h
@@ -6,17 +6,39 @@
 /*   By: sazanjan <<sazanjan@student.42heilbronn    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 16:17:03 by sazanjan          #+#    #+#             */
-/*   Updated: 2025/10/25 16:17:52 by sazanjan         ###   ########.fr       */
+/*   Updated: 2025/10/25 21:58:52 by sazanjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //make .h file for so_long project
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include "mlx.h"
-# include "libft.h"
-# include "get_next_line.h"
-# define TILE_SIZE 32
+
+# include <stdlib.h> //for malloc, free, exit
+# include <unistd.h> //for read, write, close
+# include <fcntl.h>  //for open
+# include <stdio.h>  //for printf, perror
+// # include <mlx.h>    //for mlx functions
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+#endif // BUFFER_SIZE
+
+typedef struct s_map
+{
+	char	**grid;
+	int		width;
+	int		height;
+}	t_map;
+
+
+typedef struct s_game
+{
+    t_map	map;
+}	t_game;
+
+
+void	error_exit(char *msg);
+
+
+#endif // SO_LONG_H
--- utils.c
+++ utils.c
@@ -1,28 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sazanjan <<sazanjan@student.42heilbronn    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 20:52:28 by sazanjan          #+#    #+#             */
-/*   Updated: 2025/10/26 14:12:37 by sazanjan         ###   ########.fr       */
+/*   Created: 2025/10/25 21:52:04 by sazanjan          #+#    #+#             */
+/*   Updated: 2025/10/25 21:53:30 by sazanjan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include "libft.h"
-
-static void	check_file(char *file)
-{
-	int	len;
-
-	len = ft_strlen(file);
-	if (len <= 4)
-		error_exit("Invalid file name");
-	if (ft_strncmp(&file[len - 4], ".ber", 4) != 0)
-		error_exit("Map file must have .ber extension");
-}
+#include "libft.h"    
 
 void	error_exit(char *msg)
 {
@@ -31,18 +20,3 @@ void	error_exit(char *msg)
 	ft_putstr_fd("\n", 2);
 	exit(1);
 }
-
-static void	begin_game(t_game *game, char *file)
-{
-	check_file(file);
-}
-int	main(int argc, char **argv)
-{
-	t_game	game;
-
-	if (argc != 2)
-		error_exit("Usage: ./so_long <map.ber>");
-	begin_game(&game, argv[1]);
-	return (0);
-}
-
